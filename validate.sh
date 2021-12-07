9bc6915ebb4162cd9874c3f864e808c1664adaed
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2021-12-07 08:11:59 -0500

    Remove hardcoded womtool.jar

--- validate.sh
+++ validate.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash -e
 
-java -jar ~/Applications/womtool.jar \
+if [ -z $SCING_HOME ]
+then
+    echo "Environment variable 'SCING_HOME' not defined."
+    exit 1
+fi
+
+java -jar ${SCING_HOME}/devtools/womtool.jar \
     validate \
     ArchRSA.wdl \
     --inputs ./configs/dev-fragments.inputs.aws.json
 
-java -jar ~/Applications/womtool.jar \
+java -jar ${SCING_HOME}/devtools/womtool.jar \
     validate \
     ArchRCR.wdl \
     --inputs ./configs/dev-fastq.inputs.aws.json
