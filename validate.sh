d2a6854a4303d581dca23c6f4b828dd562b5fbeb
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2021-12-10 09:34:24 -0500

    Update

--- validate.sh
+++ validate.sh
@@ -9,9 +9,9 @@ fi
 java -jar ${SCING_HOME}/devtools/womtool.jar \
     validate \
     ArchRSA.wdl \
-    --inputs ./configs/dev-fragments.inputs.aws.json
+    --inputs ./configs/template.fragments.inputs.json
 
 java -jar ${SCING_HOME}/devtools/womtool.jar \
     validate \
     ArchRCR.wdl \
-    --inputs ./configs/dev-fastq.inputs.aws.json
+    --inputs ./configs/template.fastq.inputs.json
